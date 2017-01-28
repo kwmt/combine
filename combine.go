ddb758a5c0866ce3607b652d82d60b0d71bfbfdc
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-01-28 12:36:36 +0900

    first commit

---
+++ combine.go
@@ -0,0 +1,40 @@
+package combine
+
+import (
+	"io/ioutil"
+	"log"
+	"fmt"
+	"os"
+)
+
+// dirname以下にあるファイルをfileNameファイルにまとめる
+func Combine(dirname string, fileName string) {
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fmt.Println(file.Name())
+
+		file, err := os.Open(fmt.Sprintf("%s/%s", dirname, file.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := ioutil.ReadAll(file)
+		appendToFile(fileName, string(b))
+		file.Close()
+	}
+}
+
+// filnameファイルにtextをappendする
+func appendToFile(fileName string, text string) {
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	if _, err = f.WriteString(text); err != nil {
+		panic(err)
+	}
+}
+
