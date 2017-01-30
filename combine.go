ba1123a51ea6629d19822b93d9ce02a6873dfafa
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-01-30 13:06:20 +0900

    return error

--- combine.go
+++ combine.go
@@ -10,35 +10,43 @@ import (
 )
 
 // combine the files under dirName directory
-func Combine(dirName string, fileName string) {
+func Combine(dirName string, fileName string) error {
 	files, err := ioutil.ReadDir(dirName)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	total := len(files)
 
 	for i, file := range files {
 		file, err := os.Open(fmt.Sprintf("%s/%s", dirName, file.Name()))
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		b, err := ioutil.ReadAll(file)
-		appendToFile(fileName, string(b))
+		if err != nil {
+			return err
+		}
+		err = appendToFile(fileName, string(b))
+		if err != nil {
+			return err
+		}
 
 		fmt.Printf("\r%3.0f%%", utils.Percent(i+1, total))
 		file.Close()
 	}
 	fmt.Println()
+	return nil
 }
 
 // append text to filName file
-func appendToFile(fileName string, text string) {
+func appendToFile(fileName string, text string) error {
 	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer f.Close()
 	if _, err = f.WriteString(text); err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
